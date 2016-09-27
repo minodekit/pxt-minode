411d0403616278fe57d179b76880e02dd279be17
Yongjia Niu <ahnniu@gmail.com>
2016-09-27 16:31:06 +0800

    Add cpp feature to this package

---
+++ extension.cpp
@@ -0,0 +1,8 @@
+#include "pxt.h"
+using namespace pxt;
+namespace minode {
+    //%
+    int extfun(int x, int y) {
+        return x + y;
+    }
+}
---
+++ extension.ts
@@ -0,0 +1,10 @@
+namespace minode {
+    /**
+     * Help goes here.
+     */
+    //% shim=minode::extfun
+    export function extfun(x: number, y: number) {
+        // Dummy implementation for the simulator.
+        return x - y
+    }
+}
