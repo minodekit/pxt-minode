1b38802120e48ba222f40e712cb3f453aeb51867
Yongjia Niu <ahnniu@gmail.com>
2016-10-13 11:40:50 +0800

    Add a dependency package: pxt-minode-core

--- extension.cpp
+++ extension.cpp
@@ -1,8 +1,16 @@
 #include "pxt.h"
+#include "minode-core/MiNodeComponent.h"
+#include "minode-core/MiNodeConn.h"
+
+
 using namespace pxt;
 namespace minode {
     //%
     int extfun(int x, int y) {
         return x + y;
+    }
+
+    PinName testfun2() {
+      return MiNodeConn::calcP0Name(A0);
     }
 }
