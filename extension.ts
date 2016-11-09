e4e69fe2953102a412f039f308a4d30e0c35a03f
Yongjia Niu <ahnniu@gmail.com>
2016-11-09 14:32:59 +0800

    sim test

--- extension.ts
+++ extension.ts
@@ -7,4 +7,11 @@ namespace minode {
         // Dummy implementation for the simulator.
         return x - y
     }
+
+
+
+    //% blockId=device_on_switch_event2 block="switch2 %connName| on %event"
+    export function onSwitchEvent2(connName: ConnName, event: SwitchEvent, body: Action): void {
+    	onSwitchEvent(connName, event, body);
+    } 
 }
