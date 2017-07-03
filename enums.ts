646f53255dfc8a263dc4a55235e034d11765a83e
Jerry Xia <jerry.xia@embed.com>
2017-07-03 10:58:23 +0800

    Modify for approval.

--- enums.ts
+++ enums.ts
@@ -1,69 +1,69 @@
 enum ConnName
 {
-  //% block="A0"
+  //% block="A0" enumval=0
   A0 = 0,
-  //% block="A1"
+  //% block="A1" enumval=1
   A1 = 1,
-  //% block="A2"
+  //% block="A2" enumval=2
   A2 = 2,
-  //% block="D12"
+  //% block="D12" enumval=12
   D12 = 12,
-  //% block="D13"
+  //% block="D13" enumval=13
   D13 = 13,
-  //% block="D14"
+  //% block="D14" enumval=14
   D14 = 14,
-  //% block="D15"
+  //% block="D15" enumval=15
   D15 = 15
 }
 
-declare enum AnalogConnName
+enum AnalogConnName
 {
-  //% block="A0"
+  //% block="A0" enumval=0
   Analog_A0 = 0,
-  //% block="A1"
+  //% block="A1" enumval=1
   Analog_A1 = 1,
-  //% block="A2"
+  //% block="A2" enumval=2
   Analog_A2 = 2
 }
 
-declare enum SwitchEvent
+enum SwitchEvent
 {
-//% block="open"
+//% block="open" enumval=1
 MINODE_SWITCH_EVT_OPEN = 1,
-//% block="close"
+//% block="close" enumval=2
 MINODE_SWITCH_EVT_CLOSE = 2,
 }
 
-declare enum FanStatus
+enum FanStatus
 {
-//% block="open"
+//% block="open" enumval=1
 MINODE_FAN_OPEN = 1,
-//% block="close"
+//% block="close" enumval=2
 MINODE_FAN_CLOSE = 2,
 }
 
-declare enum DHTTemStyle
+enum DHTTemStyle
 {
-//% block="Celsius"
+//% block="Celsius" enumval=1
 MINODE_DHT_CELSIUS = 1,
-//% block="Fahrenheit"
+//% block="Fahrenheit" enumval=2
 MINODE_FAN_FAHRENHEIT = 2,
 }
 
-declare enum  MiNodeColor
+enum  MiNodeColor
 {
-//% block="Red"
+//% block="Red" enumval=0
 MiNode_RGB_COLOR_RED = 0,
-//% block="Green"
+//% block="Green" enumval=1
 MiNode_RGB_COLOR_GREEN = 1,
-//% block="Blue"
+//% block="Blue" enumval=2
 MiNode_RGB_COLOR_BLUE = 2,
-//% block="Yellow"
+//% block="Yellow" enumval=3
 MiNode_RGB_COLOR_YELLOW = 3,
-//% block="Pink"
+//% block="Pink" enumval=4
 MiNode_RGB_COLOR_PINK = 4,
-//% block="Cyan"
+//% block="Cyan" enumval=5
 MiNode_RGB_COLOR_CYAN = 5,
-//% block="White"
+//% block="White" enumval=6
 MiNode_RGB_COLOR_WHITE = 6,
 }
